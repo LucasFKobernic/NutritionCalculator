b7847f2959599b4cde7f4f68e49b725057677b9e
LFK <77980399+LucasFKobernic@users.noreply.github.com>
2025-12-12 14:00:29 +0000

    chore: Add Doxyfile

--- main.cpp
+++ main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
-
+/**
+ * @brief Main execution entry point for the Nutrition Programm.
+ *
+ *
+ * @param argc The number of command-line arguments. Expects 1, 2, 3, or 4 arguments (program name included).
+ * @param argv An array of C-style strings containing the command-line arguments:
+ * - argv[1]: (Optional) ...
+ * - argv[2]: (Optional) ...
+ * - argv[3]: (Optional) ...
+ * Defaults to 'TEST'.
+ * @return int Returns 0 on successful execution. Returns 1 if a critical error occurs 
+ * (e.g., file saving failure, invalid directory path, or an error from underlying utilities).
+ */
 int main(){
     std::cout << "Starting Programm ..." << std::endl;
     return 0;
