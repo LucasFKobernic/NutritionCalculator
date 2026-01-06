df66ff2e53d7fa92c46a78e47cb6eef7c0cc0233
Lucas Fonseca <lucas.fonseca.alexandre@hotmail.com>
2026-01-06 15:54:59 +0100

    chore: update main to call Recipe.h

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+#include "Recipe.h"
 /**
  * @brief Main execution entry point for the Nutrition Programm.
  *
@@ -14,5 +16,16 @@
  */
 int main(){
     std::cout << "Starting Programm ..." << std::endl;
+    Recipe Cheesecake ("Cheesecake", 100);
+    Ingridient Mehl;
+    Mehl.name = "Mehl";
+    Mehl.amount_in_g=100;
+    Mehl.nutrition.cal_per_100=100;
+    Mehl.nutrition.protein_per_100=100;
+    Mehl.nutrition.fat_per_100=100;
+    Mehl.nutrition.carbs_per_100=100;
+
+    Cheesecake.CalculateRecipeNutrition();
+    std::cout << Cheesecake<< std::endl;
     return 0;
 }
